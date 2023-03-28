878d4461fbea32721d03a8145283cf4e18dcae73
sehyeon <boksil211@naver.com>
2023-03-29 07:05:00 +0900

    feat 조회하기 즐겨찾기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,63 +21,6 @@ function Home() {
       </StMainWrap>
       </>
     );
-  }
-
-  const queryFn = async () => {
-    const response = await axios.get("http://54.180.98.74/rooms");
-    return response.data;
-  };
-
-  const { data, isLoading, error } = useQuery(["rooms"], queryFn);
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (error) return <div>Error: {error.message}</div>;
-  return (
-    <StMainWrap>
-      <Header />
-      <Filter />
-      <StWrapperBig>
-        <FlexGap>
-          {data.map((data, index) => (
-            <Box key={data.id}>
-              <Link to={`/detail/${data.id}`}>
-                <Slider className="mainBoxWrap" {...settings} style={{}}>
-                  {data.imageList.map((imageUrl, index) => (
-                    <div className="mainBox" key={imageUrl.indexOf}>
-                      <IoIosArrowDropleft
-                        className="left arrow"
-                        color="white"
-                      />
-                      <IoIosArrowDropright
-                        className="right arrow"
-                        color="white"
-                      />
-                      <TiHeartOutline className="heart" />
-                      <TiHeart className="heart tiheart" />
-                      <div className="slick-dots"></div>
-                      <img src={imageUrl} alt="property" />
-                    </div>
-                  ))}
-                </Slider>
-              </Link>
-              <StTextWrap>
-                <StPBold>{data.title}</StPBold>
-                <StPBold>{data.address}</StPBold>
-                <StPGray>{data.description}</StPGray>
-                <StPGray>최대 인원 {data.maxGuest}</StPGray>
-                <StPBold className="paddingBottom">
-                  ₩ {data.price}
-                  <StSpanGray>/박</StSpanGray>
-                </StPBold>
-              </StTextWrap>
-            </Box>
-          ))}
-        </FlexGap>
-      </StWrapperBig>
-      <FooterMain />
-    </StMainWrap>
-  );
 }
 
 export default Home;
@@ -87,19 +30,11 @@ export const Box = styled.div`
   height: 30%;
   flex: auto;
   gap: 1%;
-`;
-export const StPBold = styled.p`
-  font-weight: 600;
-  padding: 1% 0;
-`;
-export const StPGray = styled.p`
-  color: gray;
-  padding: 2% 0;
-`;
+`
 export const StSpanGray = styled.span`
   color: gray;
   font-weight: 100;
-`;
+`
 export const StMainWrap = styled.div`
   position: relative;
 `
